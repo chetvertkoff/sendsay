15c30c3aac552469999711e386233a9519e73809
Kirill Chetvertkov <k.chetvertkov@hybrid.ai>
2020-10-13 17:21:43 +0300

    start

--- src/App/Components/AuthForm.jsx
+++ src/App/Components/AuthForm.jsx
@@ -1,9 +1,28 @@
 import React  from 'react';
+import Loader from './Loader';
 
-const AuthForm = props=>{
+const AuthForm = props => {
   return(
-    <form >
+    <form className="form auth__form">
+      <h3 className="form__title">API-консолька</h3>
+      <label className="label form__label">
+        <span className="label__text_left">Логин</span>
+        <input type="text" className="form__input-text"/>
+      </label>
+      <label className="label form__label">
+        <span className="label__text label__text_left">Сублогин</span>
+        <span className="label__text label__text_right label__text_option">Опционально</span>
+        <input type="text" className="form__input-text"/>
+      </label>
+      <label className="label form__label">
+        <span className="label__text_left">Пароль</span>
+        <input type="password" className="form__input-text form__input-text_pass"/>
+      </label>
 
+      <button onClick={event=>event.preventDefault()} className="button form__button">
+        <span className="button__text">Войти</span>
+        <Loader />
+      </button>
     </form>
   )
 }
--- src/App/Pages/Auth.jsx
+++ src/App/Pages/Auth.jsx
@@ -1,38 +1,24 @@
 import React  from 'react';
 import AuthForm from '../Components/AuthForm';
-import Loader from '../Components/Loader';
+
+const Logo = () => {
+  return (
+    <div className="logo auth__logo">
+      <div className="logo__figure logo_ellipse"></div>
+      <div className="logo__figure logo_rectangle1"></div>
+      <div className="logo__figure logo_ellipse"></div>
+      <div className="logo__figure logo_rectangle2"></div>
+    </div>
+  )
+}
 
 const Auth = ()=>{
+  
   return(
     <section className="auth">
       <div className="auth__container">
-        <div className="logo auth__logo">
-          <div className="logo__figure logo_ellipse"></div>
-          <div className="logo__figure logo_rectangle1"></div>
-          <div className="logo__figure logo_ellipse"></div>
-          <div className="logo__figure logo_rectangle2"></div>
-        </div>
-        <form className="form auth__form">
-          <h3 className="form__title">API-консолька</h3>
-          <label className="label form__label">
-            <span className="label__text_left">Логин</span>
-            <input type="text" className="form__input-text"/>
-          </label>
-          <label className="label form__label">
-            <span className="label__text label__text_left">Сублогин</span>
-            <span className="label__text label__text_right label__text_option">Опционально</span>
-            <input type="text" className="form__input-text"/>
-          </label>
-          <label className="label form__label">
-            <span className="label__text_left">Пароль</span>
-            <input type="password" className="form__input-text form__input-text_pass"/>
-          </label>
-
-          <button onClick={event=>event.preventDefault()} className="button form__button">
-            <span className="button__text">Вход</span>
-            <Loader />
-          </button>
-        </form>
+        <Logo />
+        <AuthForm />
         <a href="https://github.com/chetvertkoff/sendsay" className="link auth__link">@chetvertkoff</a>
       </div>
     </section>
