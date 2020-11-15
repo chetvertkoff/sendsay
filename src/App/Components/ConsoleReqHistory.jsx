de069ed886eb4f0e358b1434afc47b9fd4f2cccd
Kirill Chetvertkoff <chetvertkoff.kirill@gmail.com>
2020-11-15 14:26:17 +0300

    start

--- src/App/Components/ConsoleReqHistory.jsx
+++ src/App/Components/ConsoleReqHistory.jsx
@@ -1,50 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ConsoleReqHistoryItem from './ConsoleReqHistoryItem';
 import ReqDropCard from './UI/ReqDropCard';
 
 const ConsoleReqHistory = () => {
 
-  // const [style, setStyle] = useState({visibility: "hidden"});
+  const [style, setStyle] = useState({visibility: "hidden"});
+  const el = useRef(null);
 
-  const scroll = (e = {}) => { 
-    // let item = e.currentTarget || null;
-    // const optionsUI = JSON.parse(localStorage.getItem('optionsUI')) || {};
-    // if(!optionsUI.scrollPos) optionsUI.scrollPos = 0;
+  const scroll = e => { 
+    let item = el.current;
+    const optionsUI = JSON.parse(localStorage.getItem('optionsUI')) || {};
 
-    // if(!item){
-    //   item = document.querySelector('.console__req-list');
-    // }else{
-    //   const {scrollWidth, scrollLeft, offsetWidth} = item;
-    //   const pos = scrollWidth-scrollLeft-offsetWidth;
+    if(!optionsUI.scrollPos) optionsUI.scrollPos = 0;
+    if(!item.style.transform){
+      item.style.transform = `translateX(-${optionsUI.scrollPos}%)`;
+      return;
+    }
+    console.log(item.offsetWidth);
+    console.log(window.innerWidth);
+    const currentWidth = item.offsetWidth-window.innerWidth+105;
+    const direction = e.deltaY;
+    const style = window.getComputedStyle(item);
+    const matrix = new WebKitCSSMatrix(style.webkitTransform);
+    const translateX = -matrix.m41;
 
-    //   if (e.deltaY > 0) optionsUI.scrollPos += 100;
-    //   else optionsUI.scrollPos -= 100;
-    //   if(pos <= 0) optionsUI.scrollPos = scrollLeft-0.001;
-    //   if(optionsUI.scrollPos <= 0) optionsUI.scrollPos = 0;
-    // }
+    if(direction > 0) optionsUI.scrollPos = translateX+115;
+    else optionsUI.scrollPos = translateX-100;
 
-    // item.scrollLeft = optionsUI.scrollPos;
+    if(optionsUI.scrollPos > currentWidth || optionsUI.scrollPos < 0) return;
+
+    item.style.transform = `translateX(-${optionsUI.scrollPos}px)`;
     // localStorage.setItem('optionsUI', JSON.stringify({...optionsUI, scrollPos: optionsUI.scrollPos}));
-    console.log(e);
   }
 
-  // const disableDocScroll = e =>{
-  //   const scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
-  //   const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+  const disableDocScroll = e =>{
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
+    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
 
-  //   window.onscroll = ()=>{ 
-  //     window.scrollTo(scrollLeft, scrollTop); 
-  //   };
-  // }
+    window.onscroll = ()=>{ 
+      window.scrollTo(scrollLeft, scrollTop); 
+    };
+  }
 
-  // const enableDocScroll = e => {
-  //   window.onscroll=()=>{}
-  // }
+  const enableDocScroll = e => {
+    window.onscroll=()=>{}
+  }
 
-  // useEffect(()=>{
-  //   scroll();
-  //   setStyle({visibility: "inherit"});
-  // },[])
+  useEffect(()=>{
+    scroll();
+    setStyle({visibility: "inherit"});
+  },[])
 
 
 
@@ -53,15 +58,16 @@ const ConsoleReqHistory = () => {
     <div className="console__req-history console_block">
       <ul 
         className="console__req-list" 
-        // onMouseEnter={disableDocScroll} 
-        // onMouseLeave={enableDocScroll} 
+        onMouseEnter={disableDocScroll} 
+        onMouseLeave={enableDocScroll} 
         onWheel={scroll}
-        // style={style}
+        style={style}
+        ref={el}
       >
       {
         [1,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10].map((el,i)=>{
           return (
-            <ConsoleReqHistoryItem key={i} />
+            <ConsoleReqHistoryItem key={i} i={i} />
           )
         })
       }
--- src/App/Components/ConsoleReqHistoryItem.jsx
+++ src/App/Components/ConsoleReqHistoryItem.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
-const ConsoleReqHistoryItem = () => {
+const ConsoleReqHistoryItem = ({i}) => {
   const [showDrop, setDrop] = useState(false);
 
   return (
     <li className="console__req-item req-item console__req-item_success">
+      {i}
       track.get
       <svg className="icon console__drop-icon">
         <use xlinkHref="/assets/icon/sprite.svg#req-item-drop"></use>
