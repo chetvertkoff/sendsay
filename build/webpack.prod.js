eea4cc74e1366ae5eea402cfe63013e594784cdc
Kirill Chetvertkoff <chetvertkoff.kirill@gmail.com>
2021-03-30 23:28:30 +0300

    update

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -34,7 +34,11 @@ const buildWebpackConfig = merge(baseWebpackConfig, {
         {
           loader: 'css-loader',
           options: { sourceMap: true }
-        }, {
+        },
+        {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        },{
           loader: 'postcss-loader',
           options: { sourceMap: true, postcssOptions: { config: `./build/postcss.config.js` } }
         }
